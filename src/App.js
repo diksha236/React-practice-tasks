eb450ca2d1edee5ff29277adc0199217d88ba704
Diksha sathe <diksha.sathe@morrisonsplc.co.uk>
2024-06-04 15:35:15 +0530

    added my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, {
+	useState
+}
+from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const FormExample = () => {
 
-export default App;
+	const [name, setName] = useState('');
+	const [surname, setSurname] = useState('');
+
+	// Event handlers to update state variables
+	const handleNameChange = (event) => {
+		setName(event.target.value);
+	};
+
+	const handleSurnameChange = (event) => {
+		setSurname(event.target.value);
+	};
+
+
+	const handleSubmit = (event) => {
+		// Prevent default form submission
+		event.preventDefault();
+    alert("submitted Successfully")
+	
+		console.log('Name:', name);
+		console.log('surname:', surname);
+	};
+
+	return (
+		<div className="form-container">
+			<h2>Input Form</h2>
+			<form onSubmit={handleSubmit}>
+				<div className="form-group">
+					<label>Name:</label>
+					<input
+						type="text"
+						value={name}
+						onChange={handleNameChange}required
+					/>
+				</div>
+				<div className="form-group">
+					<label>surname:</label>
+					<input
+						type="text"
+						value={surname}
+						onChange={handleSurnameChange} required
+					/>
+				</div>
+				<input type="submit" value="Submit"></input>
+				{/* <button type="submit">Submit</button> */}
+			</form>
+		</div>
+	);
+};
+
+export default FormExample;
